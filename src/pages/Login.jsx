9f6abbc970d0a20047b6062d94be5751b55a330d
someonewhatever <yernar.jaqsylyq@gmail.com>
2025-04-01 22:07:14 +0500

    Added some scss modules, login is resetted for future connect with back

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,34 +13,7 @@ export function Login() {
     }
     return (
         <>
-            <div className="main-container">
-                <section className="container">
-                    <div className="login-container">
-                        <div className="circle circle-one"></div>
-                        <div className="form-container">
-                            <img src="https://raw.githubusercontent.com/hicodersofficial/glassmorphism-login-form/master/assets/illustration.png" alt="illustration" className="illustration" />
-                            <h1 className="opacity">LOGIN</h1>
-                            <form>
-                                <input type="text" 
-                                placeholder="USERNAME" 
-                                value={user.email}
-                                onChange={(e) => setUser({...user, email: e.target.value})}/>
-                                <input type="password" 
-                                placeholder="PASSWORD" 
-                                value={user.password}
-                                onChange={(e) => setUser({...user, password: e.target.value})}  />
-                                <button className="opacity" onClick={(e) => handleLogin(e) }>SUBMIT</button>
-                            </form>
-                            <div className="register-forget opacity">
-                                <a href="">REGISTER</a>
-                                <a href="">FORGOT PASSWORD</a>
-                            </div>
-                        </div>
-                        <div className="circle circle-two"></div>
-                    </div>
-                    <div className="theme-btn-container"></div>
-                </section>
-            </div>
+            
         </>
     )
 }
